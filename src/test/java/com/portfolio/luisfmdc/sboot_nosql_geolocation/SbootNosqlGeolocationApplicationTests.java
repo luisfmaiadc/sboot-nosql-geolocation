0434a424b4eaff7721fe4903cdac8a3a8f688aa0
luisfmaiadc <lf.mcosta23@gmail.com>
2025-08-26 19:31:03 -0300

    Commit inicial do projeto.

---
+++ src/test/java/com/portfolio/luisfmdc/sboot_nosql_geolocation/SbootNosqlGeolocationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.luisfmdc.sboot_nosql_geolocation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbootNosqlGeolocationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

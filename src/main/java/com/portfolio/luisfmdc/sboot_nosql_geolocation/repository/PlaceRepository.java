2feecdebbf7ee9daa47f9bb10e352f4d7ee82e14
luisfmaiadc <lf.mcosta23@gmail.com>
2025-09-25 22:49:26 -0300

    Remoção de métodos não utilizados no repository.

--- src/main/java/com/portfolio/luisfmdc/sboot_nosql_geolocation/repository/PlaceRepository.java
+++ src/main/java/com/portfolio/luisfmdc/sboot_nosql_geolocation/repository/PlaceRepository.java
@@ -1,16 +1,9 @@
 package com.portfolio.luisfmdc.sboot_nosql_geolocation.repository;
 
 import com.portfolio.luisfmdc.sboot_nosql_geolocation.domain.Place;
-import org.springframework.data.geo.Distance;
-import org.springframework.data.geo.Point;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface PlaceRepository extends MongoRepository<Place, String> {
-
-    List<Place> findByNameContainingIgnoreCaseAndActiveTrue(String nome);
-    List<Place> findByLocationNear(Point point, Distance distance);
 }
